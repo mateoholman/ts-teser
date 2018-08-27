eca39226580b0741057308ef9612c52a35b844d2
Matthew Holman <mateoholman@gmail.com>
2018-08-26 20:03:27 -0500

    Add action creators

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,3 +1,5 @@
+// This const/type pattern allows us to use TypeScript's string literal types in an easily accessible and refactorable way.
+
 export const INCREMENT_ENTHUSIASM = 'INCREMENT_ENTHUSIASM';
 export type INCREMENT_ENTHUSIASM = typeof INCREMENT_ENTHUSIASM;
 
